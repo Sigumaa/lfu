e1bbebad2d6d94b2283555251c6a0c11291cbeb9
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-03-02 19:05:02 +0900

    feat: get top artists

--- top_artists.go
+++ top_artists.go
@@ -1,1 +1,35 @@
 package lfu
+
+import "context"
+
+type TopArtistsData struct {
+	TopArtists TopArtists `json:"topartists"`
+}
+
+type TopArtists struct {
+	Artist []TopArtist `json:"artist"`
+	Attr   Attr        `json:"@attr"`
+}
+
+type TopArtist struct {
+	Streamable string        `json:"streamable"`
+	Image      []Image       `json:"image"`
+	Mbid       string        `json:"mbid"`
+	URL        string        `json:"url"`
+	Playcount  string        `json:"playcount"`
+	Attr       TopArtistAttr `json:"@attr"`
+	Name       string        `json:"name"`
+}
+
+type TopArtistAttr struct {
+	Rank string `json:"rank"`
+}
+
+func (c *Client) TopArtists(ctx context.Context, page int) (*TopArtistsData, error) {
+	url := buildURL(c.baseURL, "user.gettopartists", c.userName, c.apiKey, page)
+	var result TopArtistsData
+	if err := c.get(ctx, url, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
