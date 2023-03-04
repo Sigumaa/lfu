441116b7e3952ae111c70c6c8519c8206b306707
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-03-05 00:24:08 +0900

    feat: get now playing

--- recent_tracks.go
+++ recent_tracks.go
@@ -1,6 +1,9 @@
 package lfu
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type RecentTracksData struct {
 	RecentTracks RecentTracks `json:"recenttracks"`
@@ -49,4 +52,90 @@ func (c *Client) RecentTracks(ctx context.Context, opts ...RequestOption) (*Rece
 		return nil, err
 	}
 	return &result, nil
+}
+
+// NowPlayingTrack struct is a subset of RecentTrack struct.
+type NowPlayingTrack struct {
+	Artist     RecentTrackArtist
+	Streamable string
+	Image      []Image
+	Mbid       string
+	Album      RecentTrackAlbum
+	Name       string
+	URL        string
+	Date       Date
+}
+
+// NowPlayingTrack returns the currently playing track.
+func (c *Client) NowPlayingTrack(ctx context.Context) (*NowPlayingTrack, error) {
+	url := buildURL(c.baseURL, "user.getrecenttracks", c.userName, c.apiKey)
+	var result RecentTracksData
+	if err := c.get(ctx, url, &result); err != nil {
+		return nil, err
+	}
+	var npt NowPlayingTrack
+	if result.RecentTracks.Track[0].Attr.NowPlaying == "true" {
+		npt.Artist = result.RecentTracks.Track[0].Artist
+		npt.Streamable = result.RecentTracks.Track[0].Streamable
+		npt.Image = result.RecentTracks.Track[0].Image
+		npt.Mbid = result.RecentTracks.Track[0].Mbid
+		npt.Album = result.RecentTracks.Track[0].Album
+		npt.Name = result.RecentTracks.Track[0].Name
+		npt.URL = result.RecentTracks.Track[0].URL
+		npt.Date = result.RecentTracks.Track[0].Date
+		return &npt, nil
+	}
+	return nil, errors.New("no track is currently playing")
+}
+
+// NowPlayingTrack returns the currently playing track.
+func (r *RecentTracksData) NowPlayingTrack() (*NowPlayingTrack, error) {
+	var npt NowPlayingTrack
+	if r.RecentTracks.Track[0].Attr.NowPlaying == "true" {
+		npt.Artist = r.RecentTracks.Track[0].Artist
+		npt.Streamable = r.RecentTracks.Track[0].Streamable
+		npt.Image = r.RecentTracks.Track[0].Image
+		npt.Mbid = r.RecentTracks.Track[0].Mbid
+		npt.Album = r.RecentTracks.Track[0].Album
+		npt.Name = r.RecentTracks.Track[0].Name
+		npt.URL = r.RecentTracks.Track[0].URL
+		npt.Date = r.RecentTracks.Track[0].Date
+		return &npt, nil
+	}
+	return nil, errors.New("no track is currently playing")
+}
+
+// ArtistName returns the name of the artist.
+func (n *NowPlayingTrack) ArtistName() string {
+	return n.Artist.Text
+}
+
+// ArtistMbid returns the Mbid of the artist.
+func (n *NowPlayingTrack) ArtistMbid() string {
+	return n.Artist.Mbid
+}
+
+// GetImageURL returns the URL of the image. small, medium, large, extralarge.
+func (n *NowPlayingTrack) GetImageURL(size imageSize) string {
+	return n.Image[size].Text
+}
+
+// AlbumName returns the name of the album.
+func (n *NowPlayingTrack) AlbumName() string {
+	return n.Album.Text
+}
+
+// AlbumMbid returns the Mbid of the album.
+func (n *NowPlayingTrack) AlbumMbid() string {
+	return n.Album.Mbid
+}
+
+// GetDateText returns the date of the track.
+func (n *NowPlayingTrack) GetDateText() string {
+	return n.Date.Text
+}
+
+// GetDateUTS returns the UTS of the track.
+func (n *NowPlayingTrack) GetDateUTS() string {
+	return n.Date.UTS
 }
